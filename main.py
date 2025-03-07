db66c2d8378100fc75e8348680b592f46780ab0a
Avik-ops <avikofficial1234@gmail.com>
2025-03-07 10:04:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import tkinter.ttk as ttk
+from tkinter import filedialog
+
+window = Tk()
+
+#set window title , size, bg color 
+window.title("Image Steganogrpahy")
+window.geometry("500x500")
+window.config(background = "white")
+
+
+
+window.mainloop()
